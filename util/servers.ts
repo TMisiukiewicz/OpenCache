2771ead61aea8c414b45f7aa478d08303c8d9e02
Tomasz Misiukiewicz <t.misiukiewicz@gmail.com>
2020-01-05 18:36:54 +0100

    basic config + file of homescreen component

---
+++ util/servers.ts
@@ -0,0 +1,10 @@
+const servers = [
+  'http://opencaching.pl',
+  'http://opencaching.de',
+  'http://opencaching.us',
+  'http://opencaching.nl',
+  'http://opencaching.ro',
+  'http://opencaching.uk',
+];
+
+export default servers;
