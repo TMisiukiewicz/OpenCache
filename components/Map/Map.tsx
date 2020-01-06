ae8afce9f9cbd7ca76170db8ac6dd3024b60dfb2
Tomasz Misiukiewicz <t.misiukiewicz@gmail.com>
2020-01-06 16:39:37 +0100

    rewrite map component to class

--- components/Map/Map.tsx
+++ components/Map/Map.tsx
@@ -1,39 +1,64 @@
-import React, {useState, useEffect} from 'react';
+import React, {Component} from 'react';
 import {View, StyleSheet, Dimensions} from 'react-native';
 import MapboxGL from '@react-native-mapbox-gl/maps';
 import {GeolocationService} from 'services';
+import {MapProps, MapState} from 'propsTypes';
 
-export default function Map() {
-  const [locationPermissionGranted, setLocationPermissionGranted] = useState(
-    false,
-  );
-  const LocationService = new GeolocationService();
+export default class Map extends Component<MapProps, MapState> {
+  state: MapState;
+  LocationService: GeolocationService;
+  _map: any;
+  _camera: any;
 
-  useEffect(() => {
-    getLocation();
-  });
+  constructor(props: MapProps) {
+    super(props);
+    this.state = {
+      locationPermissionGranted: false,
+    };
 
-  const getLocation = async () => {
-    const hasLocationPermission = await LocationService.hasLocationPermission();
-    setLocationPermissionGranted(hasLocationPermission);
+    this.LocationService = new GeolocationService();
+  }
+
+  componentDidMount() {
+    this.getLocation();
+  }
+
+  getLocation = async () => {
+    const hasLocationPermission = await this.LocationService.hasLocationPermission();
+    this.setState({locationPermissionGranted: hasLocationPermission});
   };
-  return (
-    <View style={styles.container}>
-      <MapboxGL.MapView
-        style={styles.map}
-        zoomEnabled
-        rotateEnabled={false}
-        userTrackingMode={1}
-        logoEnabled={false}>
-        <MapboxGL.Camera
-          zoomLevel={14}
-          followUserLocation
-          followUserMode="compass"
-        />
-        {locationPermissionGranted && <MapboxGL.UserLocation />}
-      </MapboxGL.MapView>
-    </View>
-  );
+
+  flyToLocation = () => {
+    // console.log(this._map);
+    // this._camera.flyTo([23.0879727, 53.1210511], 2000);
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <MapboxGL.MapView
+          ref={c => {
+            this._map = c;
+          }}
+          style={styles.map}
+          zoomEnabled
+          onDidFinishLoadingMap={this.flyToLocation}
+          rotateEnabled={false}
+          userTrackingMode={1}
+          logoEnabled={false}>
+          <MapboxGL.Camera
+            ref={c => {
+              console.log(c);
+            }}
+            zoomLevel={14}
+            followUserLocation
+            followUserMode="compass"
+          />
+          {this.state.locationPermissionGranted && <MapboxGL.UserLocation />}
+        </MapboxGL.MapView>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
---
+++ propsTypes.ts
@@ -0,0 +1,5 @@
+export interface MapProps {}
+
+export interface MapState {
+  locationPermissionGranted: boolean;
+}
